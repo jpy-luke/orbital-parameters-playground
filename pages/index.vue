2617756d801b57e799bb4e42756e4ff49fd3138f
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2021-10-02 20:14:16 +0300

    feat: add controls for orbital parameters

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,40 @@
     <label>emitted fps {{ callbackFps }}</label><br/>
     <button @click="animate">Animate</button>
     <button @click="stop">Stop</button>
+    <table>
+      <thead>
+      <tr>
+        <th>Semi-major axis</th>
+        <th>Eccentricity</th>
+        <th>Inclination (rad)</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td><input @change="setSemimajorAxis" :placeholder="orbitalElements.a" type="number" step="0.1"></td>
+        <td><input @change="setEccentricity" :placeholder="orbitalElements.e" type="number" step="0.1"></td>
+        <td><input @change="setInclination" :placeholder="orbitalElements.i" type="number" step="0.1"></td>
+      </tr>
+      </tbody>
+    </table>
+    <table>
+      <thead>
+      <tr>
+        <th>Ascending node (rad)</th>
+        <th>Argument of periapsis (rad)</th>
+        <th>Mean anomaly at epoch (rad)</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td><input @change="setLongitudeOfAscendingNode" :placeholder="orbitalElements.o" type="number" step="0.1"></td>
+        <td><input @change="setArgumentOfPeriapsis" :placeholder="orbitalElements.w" type="number" step="0.1"></td>
+        <td><input @change="setMeanAnomalyAtEpoch" :placeholder="orbitalElements.m" type="number" step="0.1"></td>
+      </tr>
+      </tbody>
+    </table>
     <render-canvas @callbackFps="callbackFpsReceived" :position="position"
-                   style="width: 100%; height: 600px;"/>
+                   style="width: 100%; height: 400px;"/>
   </div>
 </template>
 
@@ -28,23 +60,46 @@ export default Vue.extend({
         z: 0,
       },
       day: 0,
-      orbit: new utils.KeplerianOrbit({
+      orbitalElements: {
         a: 2.447,
         e: 0.269,
         i: 0.04974188368183839,
         o: 2.1395991300198487,
         w: 3.312111321509639,
         m: 0.9262462340333908
-      })
+      }
     };
   },
   methods: {
+    setSemimajorAxis(event) {
+      this.orbitalElements.a = Number(event.target.value)
+    },
+    setEccentricity(event) {
+      this.orbitalElements.e = Number(event.target.value)
+      this.refresh();
+    },
+    setInclination(event) {
+      this.orbitalElements.i = Number(event.target.value)
+      this.refresh();
+    },
+    setLongitudeOfAscendingNode(event) {
+      this.orbitalElements.o = Number(event.target.value)
+      this.refresh();
+    },
+    setArgumentOfPeriapsis(event) {
+      this.orbitalElements.w = Number(event.target.value)
+      this.refresh();
+    },
+    setMeanAnomalyAtEpoch(event) {
+      this.orbitalElements.m = Number(event.target.value)
+      this.refresh();
+    },
     callbackFpsReceived(fps) {
       this.callbackFps = fps;
     },
     animate() {
       const interval = setInterval(() => {
-        this.position = this.orbit.getPositionAtTime(this.day);
+        this.refresh();
         this.day += 25
       }, 40)
       this.interval = interval;
@@ -53,11 +108,15 @@ export default Vue.extend({
       if (this.interval) {
         clearInterval(this.interval)
       }
+    },
+    refresh() {
+      const orbit = new utils.KeplerianOrbit(this.orbitalElements)
+      this.position = orbit.getPositionAtTime(this.day)
     }
 
   },
   mounted() {
-
+    this.refresh();
   }
 });
 </script>
