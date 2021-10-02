e718aff2540ff577db9278e7ec111b55eea8b4b4
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2021-10-02 09:05:36 +0300

    feat: add style to make the content a little bit spaced

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,19 @@
 <template>
   <div id="app">
-    <h2>Bar Chart Example</h2>
     <render-canvas style="width: 100%; height: 600px;" />
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import RenderCanvas from '../components/RenderCanvas.vue';
-import MyBox from '../components/MyBox.vue';
 
-export default {
+export default Vue.extend({
   name: 'app',
   components: {
     RenderCanvas
-  },
-
-  data() {
-    return {
-      chartValues: [
-        { val: 24, color: 'red' },
-        { val: 32, color: '#0f0' },
-        { val: 66, color: 'rebeccapurple' },
-        { val: 1, color: 'green' },
-        { val: 28, color: 'blue' },
-        { val: 60, color: 'rgba(150, 100, 0, 0.2)' },
-      ],
-    };
-  },
-
-  mounted() {
-    let dir = 1;
-    let selectedVal = Math.floor(Math.random() * this.chartValues.length);
-
-    setInterval(() => {
-      if (Math.random() > 0.995) dir *= -1;
-      if (Math.random() > 0.99)
-        selectedVal = Math.floor(Math.random() * this.chartValues.length);
-
-      this.chartValues[selectedVal].val = Math.min(
-        Math.max(this.chartValues[selectedVal].val + dir * 0.5, 0),
-        100
-      );
-    }, 16);
-  },
-};
+  }
+});
 </script>
 
 <style>
