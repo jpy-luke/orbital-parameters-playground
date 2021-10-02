ebb176236025604860397bef266f871a0f2d9cd3
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2021-10-02 13:24:46 +0300

    style: sanitize typescript

--- scenes/MainScene.ts
+++ scenes/MainScene.ts
@@ -24,12 +24,12 @@ const sceneObject = {
 
     new HemisphericLight("light", new Vector3(0, 0, -100), scene);
 
-    const adalia = MeshBuilder.CreateSphere("adalia", scene);
-    const materialAdalia = new StandardMaterial(("adalia-material", scene));
+    const adalia = MeshBuilder.CreateSphere("adalia", {}, scene);
+    const materialAdalia = new StandardMaterial("adalia-material", scene);
     materialAdalia.ambientColor = Color3.White();
     adalia.scaling = new Vector3(0.5, 0.5, 0.5)
 
-    const asteroid = MeshBuilder.CreateSphere("asteroid", scene);
+    const asteroid = MeshBuilder.CreateSphere("asteroid", {}, scene);
     const materialRed = new StandardMaterial("asteriod-material", scene);
     materialRed.diffuseColor = Color3.Red();
     asteroid.material = materialRed;
@@ -50,7 +50,7 @@ const sceneObject = {
 
     });
   },
-  setPosition: function (name, x, y, z) {
+  setPosition: function (name: string, x: number, y: number, z: number) {
     const mesh = sceneObject.scene.getMeshByName(name);
     if (mesh) {
       mesh.position = new Vector3(x, y, z);
