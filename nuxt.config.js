2e97d4716cff520eb09a0c5bad4e492229defa12
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2021-10-02 21:10:58 +0300

    chore: rename project

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'influence-viewer',
+    title: 'orbital-parameters-playground',
     htmlAttrs: {
       lang: 'en'
     },
